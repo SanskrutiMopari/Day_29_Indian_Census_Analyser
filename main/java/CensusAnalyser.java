380a9b7bc613aa7fc9d125efb47d16f247bf7441
Sanskruti Mopari <sanskrutimopari2016@gmail.com>
2023-01-12 10:08:51 +0530

    Indian Census Analyser

--- main/java/CensusAnalyser.java
+++ main/java/CensusAnalyser.java
@@ -1,5 +1,29 @@
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.stream.StreamSupport;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 public class CensusAnalyser {
-    public static void main(String[] args) {
-        System.out.println("Welcome");
+   private Reader reader = null;
+    public int loadIndiaCensusData(String csvFilePath) {
+
+        try {
+            reader = Files.newBufferedReader(Paths.get(csvFilePath));
+            CsvToBean<IndiaCensusCSV> csvToBean = new CsvToBeanBuilder<IndiaCensusCSV>(reader)
+                    .withType(IndiaCensusCSV.class).withIgnoreLeadingWhiteSpace(true).build();
+            Iterator<IndiaCensusCSV> iterator = csvToBean.iterator();
+            /**
+             * iterator doesn't consume memory
+             */
+            Iterable<IndiaCensusCSV> csvIterable = () -> iterator;
+            int count = (int) StreamSupport.stream(csvIterable.spliterator(), true).count();
+            return count;
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        return 0;
     }
 }
